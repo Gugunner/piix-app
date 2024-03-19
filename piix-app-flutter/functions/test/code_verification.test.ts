fb1e9c111260ef0225e5120196c70dd423993ded
Raul Alonzo Mendoza <ram2489@gmail.com>
2024-03-19 10:42:45 -0500

    Firebase functions/test send verification code (#46)
    * feat: Add unit tests for sendVerificationCodeRequest function
    
    Added unit tests to ensure sendVerificationCodeRequest function behaves as expected in various scenarios.
    
    - Test valid email and languageCode: Returns status code 200 and response body with code 0
    - Test undefined request body: Throws AppException with "invalid-body" errorCode
    - Test missing email in request body: Throws AppException with "invalid-body-fields" errorCode
    - Test missing languageCode in request body: Throws AppException with "invalid-body-fields" errorCode
    - Test firestore "set" method failure: Throws AppException with "document-not-added" errorCode
    
    These tests provide thorough coverage for the sendVerificationCodeRequest function to ensure reliability and error handling.
    
    * refactor: Add firestore property in mockObjectProperty function
    
    Updated the mockObjectProperty function to include a 'firestore' property when mocking the admin object. This change ensures that the firestore property is handled correctly in the function for better functioning.

---
+++ piix-app-flutter/functions/test/code_verification.test.ts
@@ -0,0 +1,120 @@
+import * as admin from 'firebase-admin';
+import * as myFunctions from '../src/index';
+import * as express from 'express';
+import { Request } from 'firebase-functions/v2/https';
+import { mockObjectProperty } from './utils/mock_objects';
+import { AppException } from '../src/exception/app_exception';
+import { SubModule } from '../src/exception/modules';
+
+//Tests for the sendVerificationCodeRequest function
+describe('Send Verification Code Request', () => {
+    const email = 'example@gmail.com';
+    const languageCode = 'en';
+    //Creates a spy that check the declared method sendVerificationCodeRequest
+    const spyRequest = jest.spyOn(myFunctions, 'sendVerificationCodeRequest');
+    //Clear the mocks
+   afterEach(() => {
+    jest.clearAllMocks();
+   });
+    it(`WHEN a valid email and languageCode is received
+    THEN the response status code will be a 200
+    AND the returned response body will be a code: 0`, async () => {
+        const firestore = jest.fn(() => ({
+            collection: jest.fn(() => ({
+                doc: jest.fn(() => ({
+                    set: jest.fn
+                })),
+            }))
+        }));
+        //Add the firestore to the admin mocked object
+        mockObjectProperty(admin, firestore, 'firestore');
+        const req = { body: { email: email, languageCode: languageCode } };
+        const res = {
+            status: (code: any) => {
+               expect(code).toStrictEqual(200);
+               //Return from inside to mock this
+               return {
+                send: (body: any) => {
+                    expect(body).toStrictEqual({ code: 0 });
+                }
+               }
+            },
+        }
+        //Await the call for Promises
+        await myFunctions.sendVerificationCodeRequest(req as Request, (res as unknown) as express.Response);
+        expect(spyRequest).toHaveBeenCalled();
+        expect(admin.firestore).toHaveBeenCalledTimes(1);
+    });
+
+    it(`WHEN the request body is undefined
+    THEN throw an AppException with "invalid-body" errorCode `, async () => {
+        const req = {};
+        const res = {};
+        try {
+            await myFunctions.sendVerificationCodeRequest(req as Request, (res as unknown) as express.Response);
+        } catch (e) {
+            expect(e).toBeInstanceOf(AppException);
+            const subModule = (e as AppException).details as SubModule;
+            expect(subModule.errorCode).toBe('invalid-body');
+        }
+        expect(spyRequest).toHaveBeenCalled();
+        expect(admin.firestore).toHaveBeenCalledTimes(0);
+    });
+
+    it(`WHEN the request body does not have an email
+    THEN throw an AppException with "invalid-body-fields" errorCode `, async () => {
+        const req = { body: { languageCode: languageCode } };
+        const res = {};
+        try {
+            await myFunctions.sendVerificationCodeRequest(req as Request, (res as unknown) as express.Response);
+        } catch (e) {
+            expect(e).toBeInstanceOf(AppException);
+            const subModule = (e as AppException).details as SubModule;
+            expect(subModule.errorCode).toBe('invalid-body-fields');
+        }
+        expect(spyRequest).toHaveBeenCalled();
+        expect(admin.firestore).toHaveBeenCalledTimes(0);
+    });
+
+    it(`WHEN the request body does not have a languageCode
+    THEN throw an AppException with "invalid-body-fields" errorCode `, async () => {
+        const req = { body: { email: email } };
+        const res = {};
+        try {
+            await myFunctions.sendVerificationCodeRequest(req as Request, (res as unknown) as express.Response);
+        } catch (e) {
+            expect(e).toBeInstanceOf(AppException);
+            const subModule = (e as AppException).details as SubModule;
+            expect(subModule.errorCode).toBe('invalid-body-fields');
+        }
+        expect(spyRequest).toHaveBeenCalled();
+        expect(admin.firestore).toHaveBeenCalledTimes(0);
+    });
+
+    it(`WHEN firestore "set" method fails
+    THEN throw an AppException with 'document-not-added errorCode`, async () => {
+        const req = { body: { email: 'examples@gmail.com', languageCode: 'en' } };
+        const res = {};
+        const firestore = jest.fn(() => ({
+            collection: jest.fn(() => ({
+                doc: jest.fn(() => ({
+                    //Mock the set method to throw an error
+                    set: jest.fn().mockRejectedValue(new Error('mock error'))
+                })),
+            }))
+        }));
+        //Add the firestore to the admin mocked object
+        mockObjectProperty(admin, firestore, 'firestore');
+        //Await the call for Promises
+        try {
+            await myFunctions.sendVerificationCodeRequest(req as Request, (res as unknown) as express.Response);
+        } catch (e) {
+            expect(e).toBeInstanceOf(AppException);
+            const subModule = (e as AppException).details as SubModule;
+            expect(subModule.errorCode).toBe('document-not-added');
+        }
+        expect(spyRequest).toHaveBeenCalled();
+    });
+});
+
+
