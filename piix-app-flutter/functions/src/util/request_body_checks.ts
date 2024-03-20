62783862e0c02d8c21753341a78ebf99b5789f0a
Gugunner <ram2489@gmail.com>
2024-03-20 09:05:54 -0500

    feat: Add functions to check for empty body and body fields
    Added two new functions to the codebase:
    1. checkEmptyBody() - Checks if the body of the request is undefined and throws an AppException if it is.
    2. checkBodyFields() - Checks if the body of the request has the required fields, with an optional custom message, and throws an AppException if any required field is missing.
    
    These functions enhance the validation of request bodies in the application.

---
+++ piix-app-flutter/functions/src/util/request_body_checks.ts
@@ -0,0 +1,45 @@
+import { AppException } from "../exception/app_exception";
+
+/**
+ * Check if the body of the request is undefined
+ * 
+ * @param body The body of the request
+ * @throws AppException If the body is undefined
+ */
+export function checkEmptyBody(body: any): void {
+    if (body === undefined) {
+        throw new AppException({
+            code: 'invalid-argument',
+            errorCode: 'invalid-body',
+            message: 'The request has no body.',
+            prefix: 'piix-functions',
+        });
+    };
+}
+
+/**
+ * Check if the body of the request has the required fields.
+ * 
+ * @param body The body of the request
+ * @param fields The fields that the body must have
+ * @param message An optional message to be used in the AppException
+ * @throws AppException If the body does not have the required fields
+ */
+export function checkBodyFields(body: any, fields: Array<string>, message?: string | undefined): void {
+    fields.forEach((field) => {
+        //Check if the body has the required field
+        if (!(body as Object).hasOwnProperty(field)) {
+            var definedMessage = message;
+            //If the message is not defined, create a default message
+            if (definedMessage === undefined) {
+                definedMessage = `The body does not have the required fields [${fields.join(', ')}]`;
+            }
+            throw new AppException({
+                code: 'invalid-argument',
+                errorCode: 'invalid-body-fields',
+                message: definedMessage,
+                prefix: 'piix-functions',
+            });
+        }
+    });
+}
