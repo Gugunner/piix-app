39815943bad50b868dfbca12eda55663ad65e206
Raul Alonzo <ram2489@gmail.com>
2024-03-22 10:56:41 -0500

    Created new files with environment variables
    Created three .env files that contain the information required to run Firebase App in each of the three Firebase projects (Dev, Stage and Prod).
    
    By implementing envied the values are obfuscated for build time which makes it harder to be reversed engineer.
    
    The environment classes all derive from an interface to ensure that all classes have the same properties and prevent errors when executing them, this form allows to instead detect if the environment class is of type Env and reuse the same logic where the environment values are read.

---
+++ piix-app-flutter/lib/env/env_interface.dart
@@ -0,0 +1,15 @@
+
+///Declared interface should be implemented by a real CustomEnv class 
+///with actual values.
+interface class Env {
+  ///The actual api key used to access Firebase project
+  final String apiKey = '';
+  ///The actual app id of the Firebase project
+  final String appId = '';
+  ///A special id used for communicating with the Firebase project
+  final String messageSenderId = '';
+  ///The id of the Firebase project
+  final String projectId = '';
+  ///The storage bucket of the Firebase project
+  final String storageBucket = '';
+}
---
+++ piix-app-flutter/lib/env/env_prod.dart
@@ -0,0 +1,25 @@
+import 'package:envied/envied.dart';
+import 'package:piix_mobile/env/env_interface.dart';
+
+part 'env_prod.g.dart';
+
+/// Prod environment variables
+@Envied(path: '.env.prod')
+class ProdEnv implements Env {
+  //** Firebase environment variables */
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
+  @override
+  final String apiKey = _ProdEnv.apiKey;
+  @EnviedField(varName: 'APP_ID', obfuscate: true)
+  @override
+  final String appId = _ProdEnv.appId;
+  @EnviedField(varName: 'MESSAGE_SENDER_ID', obfuscate: true)
+  @override
+  final String messageSenderId = _ProdEnv.messageSenderId;
+  @EnviedField(varName: 'PROJECT_ID', obfuscate: true)
+  @override
+  final String projectId = _ProdEnv.projectId;
+  @EnviedField(varName: 'PROJECT_ID')
+  @override
+  final String storageBucket = _ProdEnv.storageBucket;
+}
