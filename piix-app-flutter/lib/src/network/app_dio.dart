727974e921874aec277e59729fb42d4137b2f378
Raul Alonzo <ram2489@gmail.com>
2024-04-01 14:02:22 -0500

    Create Wrapper Dio class and the AppException class
    The app must handle exceptions when they occur, but instead of working with Exception classes or DioExceptionClasses an AppException class is being defined which will build from any type of exception but will work with cases when a DioException occurs.
    
    To make sure that only AppException are thrown when using dio an AppDio wrapper class was created which calls the most used HTTP requests post, put, get and delete. Where the exception is caught and transformed to an AppException subclass. And to consider any retries needed because the id token expired the general request dio method is also wrapped to ensure that always an AppException is thrown.
    
    Later the AppException error code or type will work to show the appropriate error. Note: that the AppException subclasses may grow as required.

---
+++ piix-app-flutter/lib/src/network/app_dio.dart
@@ -0,0 +1,278 @@
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:piix_mobile/app_bootstrap.dart';
+import 'package:piix_mobile/src/network/app_exception.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:dio/dio.dart';
+
+part 'app_dio.g.dart';
+
+///The Dio wrapper class for the application
+class AppDio {
+  ///Constructor for the AppDio class
+  const AppDio(this._dio);
+
+  final Dio _dio;
+
+  ///Getter for the Dio instance
+  Dio get dio => _dio;
+
+  ///Configures the Dio instance with the base url
+  void configureDio(String baseUrl) {
+    _dio
+      ..options.baseUrl = baseUrl
+      ..options.connectTimeout = const Duration(seconds: 30)
+      ..options.receiveTimeout = const Duration(seconds: 30)
+      ..options.headers = {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        HttpHeaders.authorizationHeader: 'Bearer fake_token'
+      }
+      //Add the interceptor to the Dio instance
+      ..interceptors.add(
+        _AppDioInterceptor(FirebaseAuth.instance),
+      );
+  }
+
+  ///Performs a POST request that wraps the Dio post method
+  ///to handle exceptions and errors
+  ///as CustomAppException and UnkownErrorException
+  Future<Response<T>> post<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    void Function(int, int)? onSendProgress,
+    void Function(int, int)? onReceiveProgress,
+  }) async {
+    try {
+      //Perform the post request
+      return await _dio.post<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+    } on DioException catch (dioException) {
+      //Catch the DioException and throw a CustomAppException
+      throw CustomAppException.fromModulesException(dioException);
+    } catch (error) {
+      //Catch any other error and throw an UnkownErrorException
+      //TODO: Log the error to a logging service
+      throw UnkownErrorException(error);
+    }
+  }
+
+  ///Performs a PUT request that wraps the Dio put method
+  ///to handle exceptions and errors
+  ///as CustomAppException and UnkownErrorException
+  Future<Response<T>> put<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    void Function(int, int)? onSendProgress,
+    void Function(int, int)? onReceiveProgress,
+  }) async {
+    try {
+      //Perform the put request
+      return await _dio.put<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+    } on DioException catch (dioException) {
+      //Catch the DioException and throw a CustomAppException
+      throw CustomAppException.fromModulesException(dioException);
+    } catch (error) {
+      //Catch any other error and throw an UnkownErrorException
+      //TODO: Log the error to a logging service
+      throw UnkownErrorException(error);
+    }
+  }
+
+  ///Performs a GET request that wraps the Dio get method
+  ///to handle exceptions and errors
+  ///as CustomAppException and UnkownErrorException
+  Future<Response<T>> get<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    void Function(int, int)? onReceiveProgress,
+  }) async {
+    try {
+      //Perform the get request
+      return await _dio.get<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      );
+    } on DioException catch (dioException) {
+      //Catch the DioException and throw a CustomAppException
+      throw CustomAppException.fromModulesException(dioException);
+    } catch (error) {
+      //Catch any other error and throw an UnkownErrorException
+      //TODO: Log the error to a logging service
+      throw UnkownErrorException(error);
+    }
+  }
+
+  ///Performs a DELETE request that wraps the Dio delete method
+  ///to handle exceptions and errors
+  ///as CustomAppException and UnkownErrorException
+  Future<Response<T>> delete<T>(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+  }) async {
+    try {
+      //Perform the delete request
+      return await _dio.delete<T>(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+      );
+    } on DioException catch (dioException) {
+      //Catch the DioException and throw a CustomAppException
+      throw CustomAppException.fromModulesException(dioException);
+    } catch (error) {
+      //Catch any other error and throw an UnkownErrorException
+      //TODO: Log the error to a logging service
+      throw UnkownErrorException(error);
+    }
+  }
+
+  ///Performs a request that wraps the Dio request method
+  Future<Response<T>> request<T>(
+    String url, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+    Options? options,
+    void Function(int, int)? onSendProgress,
+    void Function(int, int)? onReceiveProgress,
+  }) async {
+    try {
+      //Perform the request
+      return await _dio.request<T>(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+    } on DioException catch (dioException) {
+      //Catch the DioException and throw a CustomAppException
+      throw CustomAppException.fromModulesException(dioException);
+    } catch (error) {
+      //Catch any other error and throw an UnkownErrorException
+      //TODO: Log the error to a logging service
+      throw UnkownErrorException(error);
+    }
+  }
+}
+
+///The Dio interceptor class for the application
+///that adds the authorization header to the request
+///using the Firebase Auth idToken.
+///
+///It also handles the idToken expiration by retrying the request
+///with a new idToken.
+class _AppDioInterceptor extends Interceptor {
+  const _AppDioInterceptor(this._firebaseAuth);
+
+  final FirebaseAuth _firebaseAuth;
+
+  ///Adds the authorization header to the request
+  ///using the Firebase Auth idToken.
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    //Get the idToken from the Firebase Auth instance
+    final idToken = _firebaseAuth.currentUser?.getIdToken();
+    //Add the authorization header to the request
+    if (idToken != null) {
+      options.headers['authorization'] = 'Bearer $idToken';
+    }
+    //Call the next handler
+    super.onRequest(options, handler);
+  }
+  
+  ///Handles the DioException with status code 406 and 
+  ///errorCode 'id-token-expired'
+  @override
+  void onError(DioException dioException, ErrorInterceptorHandler handler) {
+    //Check if the DioException has status code 406 and
+    //errorCode 'id-token-expired'
+    if (dioException.response?.statusCode == HttpStatus.notAcceptable &&
+        dioException.response?.data['details']?['errorCode'] ==
+            'id-token-expired') {
+      //Get the new idToken from the Firebase Auth instance
+      //and retry the request
+      _firebaseAuth.currentUser!
+          .getIdToken(true)
+          .then((idToken) => _retryRequest(idToken!, dioException, handler));
+    } else {
+      super.onError(dioException, handler);
+    }
+  }
+
+  ///Retries the request with the new idToken
+  Future<void> _retryRequest(String idToken, DioException dioException,
+      ErrorInterceptorHandler handler) async {
+    //Get the request options from the DioException
+    final requestOptions = dioException.requestOptions;
+    //Add the new idToken to the authorization header
+    requestOptions.headers['authorization'] = 'Bearer $idToken';
+    //Create a new AppDio instance and configure it with the base url
+    final appDio = AppDio(Dio());
+    //Perform the request with the new idToken
+    appDio.configureDio(requestOptions.baseUrl);
+    try {
+      //Perform the request
+      final response = await appDio.request(
+        dioException.requestOptions.path,
+        data: dioException.requestOptions.data,
+        queryParameters: dioException.requestOptions.queryParameters,
+        cancelToken: dioException.requestOptions.cancelToken,
+        options: Options(
+          method: requestOptions.method,
+          headers: requestOptions.headers,
+        ),
+        onSendProgress: requestOptions.onSendProgress,
+        onReceiveProgress: requestOptions.onReceiveProgress,
+      );
+      //Resolve the handler with the response
+      handler.resolve(response);
+    } catch (dioException) {
+      //Reject the handler with the DioException
+      handler.reject(dioException as DioException);
+    }
+  }
+}
+
+@Riverpod(keepAlive: true)
+AppDio appDio(AppDioRef) {
+  final appDio = AppDio(Dio());
+  final String baseUrl = AppDioRef.read(envProvider).state?.baseUrl ?? '';
+  appDio.configureDio(baseUrl);
+  return appDio;
+}
