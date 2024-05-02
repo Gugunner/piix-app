beff5b105e90151d95d41e7d15fc9c7cedc9a8fb
Raul Alonzo <ram2489@gmail.com>
2024-05-02 12:16:00 -0500

    Add SizedTextButton
    According to the design guidelines for the text button there are three different text sizes to choose from. To avoid the hassle of creating a TextButton and then styling each ButtonStyle class depending on the sized text per TextButton widget. A subclass of the TextButton is created which has an override that by default passes the BuildContext TextButtonTheme and just switches the TextStyle inside the button. Since accentTheme does not exist because Flutter just handles two TextTheme a simple copyWith when calling the BuildContext solves the issue to instead substitute primaryTextTheme PrimaryTextStyle with the AccentTextStyle. This way all that is needed is to call a SizedTextButton and pass the SizedText value from the enumerable.

--- piix-app-flutter/lib/src/theme/theme_context.dart
+++ piix-app-flutter/lib/src/theme/theme_context.dart
@@ -1,6 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:piix_mobile/src/theme/theme_barrel_file.dart';
 
 /// An extension that provides a getter for the theme of the current context.
 extension ThemeContext on BuildContext {
   ThemeData get theme => Theme.of(this);
+
+  ThemeData get accentTheme => theme.copyWith(
+          primaryTextTheme: TextTheme(
+        displayLarge: TextAccentStyle.displayLarge.copyWith(
+          color: PiixColors.infoDefault,
+        ),
+        displayMedium: TextAccentStyle.displayMedium
+            .copyWith(color: PiixColors.infoDefault),
+        displaySmall: TextAccentStyle.displaySmall.copyWith(
+          color: PiixColors.infoDefault,
+        ),
+        headlineLarge: TextAccentStyle.headlineLarge.copyWith(
+          color: PiixColors.primary,
+        ),
+        headlineMedium: TextAccentStyle.headlineMedium.copyWith(
+          color: PiixColors.highlight,
+        ),
+        headlineSmall: TextAccentStyle.headlineSmall.copyWith(
+          color: PiixColors.active,
+        ),
+        titleLarge: TextAccentStyle.titleLarge.copyWith(
+          color: PiixColors.infoDefault,
+        ),
+        titleMedium: TextAccentStyle.titleMedium.copyWith(
+          color: PiixColors.infoDefault,
+        ),
+        titleSmall: TextAccentStyle.titleSmall.copyWith(
+          color: PiixColors.highlight,
+        ),
+        labelLarge: TextAccentStyle.labelLarge.copyWith(
+          color: PiixColors.primary,
+        ),
+        labelMedium: TextAccentStyle.labelMedium.copyWith(
+          color: PiixColors.infoDefault,
+        ),
+        labelSmall: TextAccentStyle.labelSmall.copyWith(
+          color: PiixColors.primary,
+        ),
+        bodyLarge: TextAccentStyle.bodyLarge.copyWith(
+          color: PiixColors.primary,
+        ),
+        bodyMedium: TextAccentStyle.bodyMedium.copyWith(
+          color: PiixColors.infoDefault,
+        ),
+        bodySmall: TextAccentStyle.bodySmall.copyWith(
+          color: PiixColors.highlight,
+        ),
+      ));
+
+  
 }
