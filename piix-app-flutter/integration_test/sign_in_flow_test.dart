35ffcaa9922f15386c3afd3e9d311ce6751e48ff
Raul Alonzo <ram2489@gmail.com>
2024-05-06 11:14:22 -0500

    Run integration tests
    *Add a new configuration object to VSCode launch.json to run the main file inside the integration test folder.
    *Add to integration_test/main.dart a centralized main to call all integration tests from one main.
    * Add integration tests for web version to the following flows: i) create account; ii) welcome sign in; and iii) sign in. All integration tests have a Widget test version and a version that can be run on an emulated device.
    *Modified robots methods to comply with the integration test flow and allow for a more versatile approach between sign in and sign up.
    *When creating fake containers now the app can create an initial fake user to test sign in without the need of first doing the create account flow.

---
+++ piix-app-flutter/integration_test/sign_in_flow_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:piix_mobile/src/constants/widget_keys.dart';
+
+import '../test/src/robot.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('Web sign in flow', (tester) async {
+    final robot = Robot(tester);
+    await robot.pumpMyAppWithFakes(
+      isWeb: true,
+      initialUserEmail: robot.auth.testSignInEmail,
+    );
+    robot.expectToFindWelcomePage();
+    await robot.tapSignInSignUpButton();
+    robot.auth.expectSignUpPage();
+    await robot.tapSignInSignUpButton(isSignIn: false);
+    robot.auth.expectSignInPage();
+    await robot.auth.enterEmail(robot.auth.testSignInEmail);
+    await robot.auth.tapButtonByKey(widgetKey: WidgetKeys.submitEmailButton);
+    robot.auth.expectEmailVerificationCodePage();
+    await robot.auth.enterVerificationCode(checkFocus: false);
+    await robot.auth
+        .tapButtonByKey(widgetKey: WidgetKeys.submitVerificationCodeButton);
+    robot.expectToFindHomePage();
+  });
+}
---
+++ piix-app-flutter/test/src/features/authentication/presentation/sign_in_flow_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:piix_mobile/src/constants/widget_keys.dart';
+
+import '../../../robot.dart';
+
+void main() {
+  testWidgets('Web sign in flow', (tester) async {
+    final robot = Robot(tester);
+    await robot.pumpMyAppWithFakes(
+      isWeb: true,
+      initialUserEmail: robot.auth.testSignInEmail,
+    );
+    robot.expectToFindWelcomePage();
+    await robot.tapSignInSignUpButton();
+    robot.auth.expectSignUpPage();
+    await robot.tapSignInSignUpButton(isSignIn: false);
+    robot.auth.expectSignInPage();
+    await robot.auth.enterEmail(robot.auth.testSignInEmail);
+    await robot.auth.tapButtonByKey(widgetKey: WidgetKeys.submitEmailButton);
+    robot.auth.expectEmailVerificationCodePage();
+    await robot.auth.enterVerificationCode();
+    await robot.auth
+        .tapButtonByKey(widgetKey: WidgetKeys.submitVerificationCodeButton);
+    robot.expectToFindHomePage();
+  });
+}
